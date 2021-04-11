d26fc4940c48c1ad0c65ddbf9137d281fc2ae376
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-11 23:35:17 +0100

    modal

--- src/components/Products/Form.js
+++ src/components/Products/Form.js
@@ -1,2 +1,22 @@
-const Form = () => <div>Hello from Form</div>
-export default Form
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+const Form = ({ setShow }) => {
+  const modalRef = useRef();
+  const updateShow = (evt) => {
+    if (evt.target == modalRef.current) {
+      setShow();
+    }
+  };
+  return (
+    <div ref={modalRef} onClick={updateShow} className="modal">
+      <div className="card">
+        <div onClick={setShow} className="close">
+          <FontAwesomeIcon className="mx-1" icon={faTimes} />
+        </div>
+        hello from card
+      </div>
+    </div>
+  );
+};
+export default Form;
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,16 +1,19 @@
+import React, { useState } from 'react';
 import "./index.scss"
 import Form from "./Form";
 import Table from "./Table";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-const Products = () => (
+const Products = () => {
+  const [show, setShow] = useState(false);
+  return (
   <div className="products">
-    <button className="btn bg-primary">
+    <button onClick={() => setShow(true)} className="btn bg-primary">
       <FontAwesomeIcon className="mx-1" icon={faPlus} />
       New product
     </button>
-    <Form />
+    { show ? <Form setShow={setShow.bind(this,false)} /> : ""}
     <Table />
   </div>
-);
+)};
 export default Products;
