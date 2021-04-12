114eef002fae5d84caa16b9553648fb6fa2211be
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-12 02:27:34 +0100

    state

--- src/components/Products/Form.js
+++ src/components/Products/Form.js
@@ -1,20 +1,54 @@
 import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-const Form = ({ setShow }) => {
+const Form = ({ product, updateProduct, close }) => {
   const modalRef = useRef();
   const updateShow = (evt) => {
-    if (evt.target == modalRef.current) {
-      setShow();
+    if (evt.target === modalRef.current) {
+      close();
     }
+  };
+  const updatePrice = ({ target }) => {
+    let evt = {};
+    evt.target = { name: target.name, value: Number(target.value) };
+    updateProduct(evt);
   };
   return (
     <div ref={modalRef} onClick={updateShow} className="modal">
       <div className="card">
-        <div onClick={setShow} className="close">
+        <div onClick={close} className="close">
           <FontAwesomeIcon className="mx-1" icon={faTimes} />
         </div>
-        hello from card
+        <form>
+          {JSON.stringify(product)}
+          <label htmlFor="fname">Name:</label>
+          <input
+            value={product.name}
+            onChange={updateProduct}
+            type="text"
+            id="fname"
+            name="name"
+            required
+          />
+          <label htmlFor="fprice">Price:</label>
+          <input
+            value={product.price}
+            onChange={updatePrice}
+            type="number"
+            id="fprice"
+            name="price"
+            required
+          />
+          <label htmlFor="fdescription">Decription:</label>
+          <input
+            value={product.description}
+            onChange={updateProduct}
+            type="text"
+            id="fdescription"
+            name="description"
+            required
+          />
+        </form>
       </div>
     </div>
   );
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,19 +1,46 @@
-import React, { useState } from 'react';
-import "./index.scss"
+import React, { useState } from "react";
+import "./index.scss";
 import Form from "./Form";
 import Table from "./Table";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const stateDefault = {
+  action: "create",
+  show: false,
+  product: {
+    name: "",
+    price: "",
+    description: "",
+  },
+};
 const Products = () => {
-  const [show, setShow] = useState(false);
+  const [state, setState] = useState(stateDefault);
+  const updateProduct = ({ target }) =>
+    setState({
+      ...state,
+      product: { ...state.product, [target.name]: target.value },
+    });
+  const open = (action, product) => {
+    setState({ action, product, show: true });
+  };
+  const close = () => setState({ ...state, show: false });
   return (
-  <div className="products">
-    <button onClick={() => setShow(true)} className="btn bg-primary">
-      <FontAwesomeIcon className="mx-1" icon={faPlus} />
-      New product
-    </button>
-    { show ? <Form setShow={setShow.bind(this,false)} /> : ""}
-    <Table />
-  </div>
-)};
+    <div className="products">
+      <button
+        onClick={() => open("create", stateDefault.product)}
+        className="btn bg-primary"
+      >
+        <FontAwesomeIcon className="mx-1" icon={faPlus} />
+        New product
+      </button>
+      {state.show ? (
+        <Form product={state.product} updateProduct={updateProduct} close={close} />
+      ) : (
+        ""
+      )}
+      <Table />
+    </div>
+  );
+};
 export default Products;
