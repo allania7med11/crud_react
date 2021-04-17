81d7c66b5a0ad70058e2dede3d2e96809a14d279
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-17 21:36:25 +0100

    table

--- src/components/Products/Table.js
+++ src/components/Products/Table.js
@@ -1,2 +1,36 @@
-const Table = () => <div>Hello from Table</div>
-export default Table
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt, faTrash } from "@fortawesome/free-solid-svg-icons";
+const fields = [
+  { label: "Name", value: "name" },
+  { label: "Price", value: "price" },
+  { label: "Description", value: "description" },
+];
+const Product = ({ product }) => {
+  return (
+    <>
+      {fields.map((field) => (
+        <div className="header">{product[field.value]}</div>
+      ))}
+      <div>
+        <button class="btn bg-warning px-3 py-1">
+          <FontAwesomeIcon className="mx-1" icon={faPencilAlt} />
+        </button>
+        <button class="btn bg-danger px-3 py-1">
+          <FontAwesomeIcon className="mx-1" icon={faTrash} />
+        </button>
+      </div>
+    </>
+  );
+};
+const Table = ({ products }) => (
+  <div className="grid-container">
+    {fields.map((field) => (
+      <div class="header">{field.label}</div>
+    ))}
+    <div class="header">Actions</div>
+    {products.map((product) => (
+      <Product product={product} />
+    ))}
+  </div>
+);
+export default Table;
